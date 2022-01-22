88fac9a9aaefc95586c856c8d786645c920daf5b
Spisy <60527150+Spisy@users.noreply.github.com>
2022-01-22 13:04:27 -0500

    joystick commands and button configuration

--- FRC_2022_MainSeason/src/main/java/frc/robot/RobotContainer.java
+++ FRC_2022_MainSeason/src/main/java/frc/robot/RobotContainer.java
@@ -1,10 +1,31 @@
 package frc.robot;
+import frc.robot.subsystems.drivetrain.Drivetrain;
 
 public class RobotContainer {
   // Subsystems and commands
+  Drivetrain drivetrain = new drivetrain();
+  Button button = new Button();
+  button.toggleWhenPressed(new StartEndCommand(drivetrain::onMethod,
+  drivetrain::offMethod,
+  drivetrain));
 
+
+  button.whileHeld(new StartendCommand(drivetrain::onMethod,
+  drivetrain::offMethod,
+  drivetrain));
+
+  
+  Xboxcontroller xboxcontroller = new Xboxcontroller(1);
+  //create class for xbox controller 
+  
+
+  joystickButton.Button.A, new joystickButton(joystick, 1);
+
+  buttonA.whenPressed(new intake());
+  
   public RobotContainer() {
     configureButtonBindings();
+
   }
 
   private void configureButtonBindings() {}
