8022338356e176c0c8e81a3591cb380157bbca6e
Calvin Macatantan <calmacatantan@gmail.com>
2022-02-05 09:02:27 -0500

    removed brocken code </3

--- FRC_2022_MainSeason/src/main/java/frc/robot/subsystems/Climber/UseClimber.java
+++
@@ -1,6 +0,0 @@
-package frc.robot.subsystems.Climber;
-
-public class UseClimber {
-    Joystick Left = new Joystick(0);
-
-}
--- FRC_2022_MainSeason/src/main/java/frc/robot/subsystems/Climber/useClimber.java
+++
@@ -1,5 +0,0 @@
-package frc.robot.subsystems.climber;
-
-public class UseClimber {
-
-}
