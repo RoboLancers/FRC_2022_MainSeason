b98e264de4fca8771c03452346d6f2fa7b648460
MannyLev <94080012+MannyLev@users.noreply.github.com>
2022-02-11 16:17:25 -0500

    New intake changes

--- FRC_2022_MainSeason/src/main/java/frc/robot/subsystems/intake/Intake.java
+++ FRC_2022_MainSeason/src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -1,27 +1,43 @@
 package frc.robot.subsystems.intake;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import frc.robot.Constants;
+import frc.robot.Constants.Indexer;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public class Intake extends SubsystemBase{
-    public CANSparkMax rollerMotor, indexerMotor;
-    public DoubleSolenoid rectractionMotor;
+    public CANSparkMax intakeMotor; // for roller
+    public DoubleSolenoid retractionPiston; // for pistons
 
     public Intake() {
-        rollerMotor = new CANSparkMax(Constants.Intake.ROLLER_PORT, MotorType.kBrushless); //intake motor for roller
-        indexerMotor = new CANSparkMax(Constants.Intake.INDEXER_PORT, MotorType.kBrushless); //transfer motor for indexer
-        rectractionMotor = new DoubleSolenoid(PneumaticsModuleType.REVPH, Constants.Intake.RETRACTOR_CHANNEL_ONE, Constants.Intake.RETRACTOR_CHANNEL_TWO);
+        this.intakeMotor = new CANSparkMax(Constants.Intake.kIndexerPort, MotorType.kBrushless); //intake motor for roller
+        intakeMotor.setIdleMode(IdleMode.kCoast); // This means that the motor running the intake will not brake
+        this.retractionPiston = new DoubleSolenoid(PneumaticsModuleType.REVPH, Constants.Intake.kPistonDeploy, Constants.Intake.kPistonRetract);
+        retractionPiston.set(Value.kReverse);
+        // The piston needs additional logic. Something along the lines of:
+        // retractionPiston.set(Value.kReverse); 
+        // To explain, kReverse just means that the piston will go to its retracted position. kForward is the extended position
+        // Double action solenoids can use a toggle() method, but they need to be set to a position initially
     }
 
-    public CANSparkMax getRollerMotor() {return rollerMotor;}
+    public void toggle() {
+        retractionPiston.set(Value.kReverse);
+        intakeMotor.set(Constants.Intake.kIntakePower);
+    }
 
-    public CANSparkMax getIndexerMotor() {return indexerMotor;}
+    public void setPower() {
+        intakeMotor.set(Constants.Intake.kIntakePower);
+    }
 
+    // You should make a setPower function that does intakeMotor.set()
 
+    // You should make a toggle() function that calls retractionPiston.toggle()
 
+}
 }
