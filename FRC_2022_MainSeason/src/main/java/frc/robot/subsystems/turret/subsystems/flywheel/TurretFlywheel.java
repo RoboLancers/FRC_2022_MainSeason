fc45a9e1358f0b50cbde21e429fbb10003e0ff9b
Calvin Macatantan <calmacatantan@gmail.com>
2022-02-11 15:54:29 -0500

    modified turretflywheel subsystem

--- FRC_2022_MainSeason/src/main/java/frc/robot/subsystems/turret/subsystems/flywheel/TurretFlywheel.java
+++ FRC_2022_MainSeason/src/main/java/frc/robot/subsystems/turret/subsystems/flywheel/TurretFlywheel.java
@@ -3,36 +3,48 @@ package frc.robot.subsystems.turret.subsystems.flywheel;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-import com.qualcomm.robotcore.hardware;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 
 public class TurretFlywheel extends SubsystemBase {
     private CANSparkMax flywheelMotorA;
     private CANSparkMax flywheelMotorB;
     private CANSparkMax kickWheelMotor;
-    private CANEncoder flywheelEncoderA;
+
+    private RelativeEncoder flyEncoderA;
+    private RelativeEncoder flyEncoderB;
 
     public TurretFlywheel(){
-        this.flywheelMotor = new CANSparkMax(CANSparkMaxLowLevel.MotorType.kBrushless, Constants.Turret.Ports.kFlywheelMotorA);
-        this.flywheelMotor = new CANSparkMax(CANSparkMaxLowLevel.MotorType.kBrushless, Constants.Turret.Ports.kFlywheelMotorB);
-        this.kickWheelMotor = new CANSparkMax(CANSparkMaxLowLevel.MotorType.kBrushless, Constants.Turret.Ports.kKickWheel);
-        this.flywheelEncoderA = this.flywheelEncoderA.getEncoder();
-        this.flywheelEncoderB = this.flywheelEncoderB.getEncoder();
+        flywheelMotorA = new CANSparkMax(Constants.Turret.Ports.kFlywheelMotorA, CANSparkMax.MotorType.kBrushless);
+        flywheelMotorB = new CANSparkMax(Constants.Turret.Ports.kFlywheelMotorB, CANSparkMax.MotorType.kBrushless);
+        kickWheelMotor = new CANSparkMax(Constants.Turret.Ports.kKickWheel, CANSparkMax.MotorType.kBrushless);
+
+        flyEncoderA = flywheelMotorA.getEncoder();
+        flyEncoderB = flywheelMotorB.getEncoder();
+
+        flywheelMotorB.follow(flywheelMotorA);
+        
+        flywheelMotorA.setInverted(false);
+        flywheelMotorB.setInverted(true);
+
     }
 
     public double getVelocity(){
-        double speedA = this.flyEnocderA.getVelocity(); //should return in ticks per second
-        double speedB = this.flyEnocderB.getVelocity(); //should return in ticks per second
+        double speedA = this.flyEncoderA.getVelocity(); //should return in ticks per second
+        double speedB = this.flyEncoderB.getVelocity(); //should return in ticks per second
         return 0.5 * (speedA + speedB);
     }
 
-    public void setPower(double power){
-        
+    public void set(double power){
+        flywheelMotorA.set(power);
+        flywheelMotorB.set(power);
     }
 
     public boolean isUpToSpeed(double launchTrajectorySpeed){
         return Math.abs(this.getVelocity() - launchTrajectorySpeed) < Constants.Turret.TunedCoefficients.FlywheelPID.kErrorThreshold;
     }
+
 }
