d50b9bed6f598cae619fab0ae2674bdb2d7f3f38
MannyLev <94080012+MannyLev@users.noreply.github.com>
2022-02-28 16:21:45 -0500

    Initial Commit

--- FRC_2022_MainSeason/src/main/java/frc/robot/subsystems/intake/commands/UseIntake.java
+++ FRC_2022_MainSeason/src/main/java/frc/robot/subsystems/intake/commands/UseIntake.java
@@ -1,24 +1,24 @@
-package frc.robot.subsystems.intake.commands;
+// package frc.robot.subsystems.intake.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.intake.Intake;
+// import edu.wpi.first.wpilibj2.command.CommandBase;
+// import frc.robot.subsystems.intake.Intake;
 
 
-public class UseIntake extends CommandBase{ // DeployIntake
-    public Intake intake;
+// public class UseIntake extends CommandBase{ // DeployIntake
+//     public Intake intake;
 
-    public UseIntake(Intake intake) {
-        this.intake = new Intake();
-        addRequirements(intake);
-    }
+//     public UseIntake(Intake intake) {
+//         this.intake = new Intake();
+//         addRequirements(intake);
+//     }
 
-    @Override
-    public void execute() {
-        intake.toggleDeploy();; //extends pistons
-    }   
+//     @Override
+//     public void execute() {
+//         intake.toggleDeploy();; //extends pistons
+//     }   
 
-    @Override
-    public boolean isFinished() {
-        return false;
-    }
-}
+//     @Override
+//     public boolean isFinished() {
+//         return false;
+//     }
+// }
