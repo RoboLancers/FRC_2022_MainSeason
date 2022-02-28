164f9a3591dc27ebc80645915abeadec57a27564
danielismailov <7889247@philasd.org>
2022-02-28 17:48:25 -0500

    added stuff

--- FRC_2022_MainSeason/src/main/java/frc/robot/subsystems/Climber/commands/ManualClimber.java
+++ FRC_2022_MainSeason/src/main/java/frc/robot/subsystems/Climber/commands/ManualClimber.java
@@ -1,16 +1,16 @@
 package frc.robot.subsystems.climber.commands;
 import frc.robot.Constants;
 import frc.robot.subsystems.climber.Climber;
+import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.util.XboxController;
 
 public class ManualClimber extends CommandBase {
     private XboxController climberController;
-    private double manualClimb;
-
+    private final CANSparkMax climberMotor1 = new CANSparkMax(5, CANSparkMax.MotorType.kBrushless);
+    private final CANSparkMax climberMotor2 = new CANSparkMax(6, CANSparkMax.MotorType.kBrushless);
     public ManualClimber(ManualClimber ManualClimber, XboxController climberController){
         this.climberController = climberController;
-        this.manualClimb = manualClimb;
     }
 
     @Override
