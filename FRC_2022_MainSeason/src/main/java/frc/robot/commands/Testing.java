064845ca0b1b6f860506b93fb2f8bc3ee96851f7
MannyLev <94080012+MannyLev@users.noreply.github.com>
2022-03-18 15:01:03 -0400

    Now things stop

--- FRC_2022_MainSeason/src/main/java/frc/robot/commands/Testing.java
+++ FRC_2022_MainSeason/src/main/java/frc/robot/commands/Testing.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
@@ -32,34 +33,40 @@ public class Testing extends CommandBase {
                 intake.setPower(0.05);
               }, intake),
               new WaitCommand(2)),
+            new PrintCommand("intake forward success"),
             new ParallelRaceGroup(new RunCommand(() -> {
                     intake.setPower(-0.05);
                 }, intake),
                 new WaitCommand(2)),
+            new PrintCommand("intake backward success"),
             new ParallelRaceGroup(new RunCommand(() -> {
                     intake.setPower(0);
             }, intake),
+            new PrintCommand("intake off"),
             new WaitCommand(2)),
             new ParallelRaceGroup(new RunCommand(() -> {
                 indexer.setPower(0.05);    
             }, indexer), new WaitCommand(2)),
+            new PrintCommand("indexer forward success"),
             new ParallelRaceGroup(new RunCommand(() -> {
                 indexer.setPower(-0.05);    
             }, indexer), new WaitCommand(2)),
+            new PrintCommand("indexer backward success"),
             new ParallelRaceGroup(new RunCommand(() -> {
                 indexer.setPower(0);    
             }, indexer),
+            new PrintCommand("indexer off"),
             new WaitCommand(2)),
             new ParallelRaceGroup(
-                new RunCommand(() -> {
-                    new UpperHubShoot(turret);
-                }, turret),
+                new UpperHubShoot(turret),
                 new WaitCommand(2)),
+            new PrintCommand("turret success"),
             new ParallelRaceGroup(new RunCommand(() -> {
                 climber.climberMotor1.set(0.1);
                 climber.climberMotor2.set(0.1);
             }, climber),
             new WaitUntilCommand(1)),
+            new PrintCommand("climber up success"),
             /* new ParallelRaceGroup(new RunCommand(() -> {
                 intake.toggleIntake();
                 intake.toggleIntake();
@@ -69,7 +76,8 @@ public class Testing extends CommandBase {
                 climber.climberMotor1.set(-0.1);
                 climber.climberMotor2.set(-0.1);
             }, climber),
-            new WaitUntilCommand(1))
+            new WaitUntilCommand(1)),
+            new PrintCommand("climber down success")
         );
     }
 }
