51cb0eda5855f93b9ee792f0b61c9dc5f0048dcf
mattperls-code <mattperls.code@gmail.com>
2022-03-23 14:45:00 -0400

    ready to be used by driveteam, same controls/functionality as used in matches at SCH

--- FRC_2022_MainSeason/src/main/java/frc/robot/RobotContainer.java
+++ FRC_2022_MainSeason/src/main/java/frc/robot/RobotContainer.java
@@ -1,74 +1,33 @@
 package frc.robot;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.List;
 
-import frc.robot.subsystems.climber.Climber;
 import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.RamseteController;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
-import edu.wpi.first.math.geometry.Pose2d;  
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.math.trajectory.TrajectoryUtil;
-import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Filesystem;
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.XboxController.Button;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.subsystems.drivetrain.Pneumatics;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.commands.MoveForward;
 import frc.robot.commands.OneBallAuto;
-import frc.robot.commands.TaxiAuto;
 import frc.robot.commands.TwoBallAuto;
-// import frc.robot.commands.GeneralizedReleaseRoutine;
-import frc.robot.commands.UpdateLights;
 import frc.robot.commands.ZeroClimber;
+import frc.robot.subsystems.climber.Climber;
 import frc.robot.subsystems.climber.commands.ManualClimber;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.subsystems.drivetrain.GearShifter;
-import frc.robot.subsystems.drivetrain.commands.ToggleGearShifter;
+import frc.robot.subsystems.drivetrain.Pneumatics;
 import frc.robot.subsystems.drivetrain.commands.TurnToAngle;
-import frc.robot.subsystems.drivetrain.commands.UseCompressor;
-//import frc.robot.subsystems.misc.AddressableLEDs;
 import frc.robot.subsystems.indexer.Indexer;
-import frc.robot.subsystems.turret.Turret;
-//import frc.robot.subsystems.turret.commands.ActiveLaunchTrajectory;
-import frc.robot.subsystems.drivetrain.enums.GearShifterState;
-import frc.robot.subsystems.misc.AddressableLEDs;
-import frc.robot.subsystems.misc.Camera;
-
 import frc.robot.subsystems.intake.Intake;
-import frc.robot.subsystems.intake.commands.UseIntake;
-import frc.robot.subsystems.turret.LaunchTrajectory;
+import frc.robot.subsystems.misc.Camera;
+import frc.robot.subsystems.turret.Turret;
 import frc.robot.subsystems.turret.commands.ActiveLaunchTrajectory;
 import frc.robot.subsystems.turret.commands.LowHubShoot;
 import frc.robot.subsystems.turret.commands.UpperHubShoot;
 import frc.robot.subsystems.turret.commands.ZeroPitch;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.NotifierCommand;
-import edu.wpi.first.wpilibj2.command.RamseteCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.util.XboxController;
-import frc.robot.util.XboxController.Axis;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-// import frc.robot.subsystems.climber.commands.ResetClimber;
-
 
 public class RobotContainer {
   private RobotContainer m_robotContainer;
@@ -102,14 +61,9 @@ public class RobotContainer {
     this.configureButtonBindings();
 
     // A split-stick arcade command, with forward/backward controlled by the left hand, and turning controlled by the right.
-    this.drivetrain.setDefaultCommand(
-      new RunCommand(
-        () -> {
-          this.drivetrain.arcadeDrive(driverController.getAxisValue(XboxController.Axis.LEFT_Y), driverController.getAxisValue(XboxController.Axis.RIGHT_X));
-        },
-        drivetrain
-      )
-    );
+    this.drivetrain.setDefaultCommand(new RunCommand(
+      () -> this.drivetrain.arcadeDrive(driverController.getAxisValue(XboxController.Axis.LEFT_Y), driverController.getAxisValue(XboxController.Axis.RIGHT_X)),
+    drivetrain));
 
     indexer.setDefaultCommand(new RunCommand(() -> {
       indexer.setPower(manipulatorController.getAxisValue(XboxController.Axis.RIGHT_Y));
@@ -125,10 +79,6 @@ public class RobotContainer {
 
     // Shot trajectory tuning
 
-    SmartDashboard.putNumber("Angular kP", SmartDashboard.getNumber("Angular kP", Constants.Turret.Yaw.kP));
-    SmartDashboard.putNumber("Angular kI", SmartDashboard.getNumber("Angular kI", Constants.Turret.Yaw.kI));
-    SmartDashboard.putNumber("Angular kD", SmartDashboard.getNumber("Angular kD", Constants.Turret.Yaw.kD));
-
     SmartDashboard.putNumber("High Shot Speed", SmartDashboard.getNumber("High Shot Speed", 3700));
     SmartDashboard.putNumber("High Shot Angle", SmartDashboard.getNumber("High Shot Angle", 3.5));
   
@@ -138,36 +88,12 @@ public class RobotContainer {
     autoChooser.addOption("Zero Climber", new ZeroClimber(climber, turret));
     autoChooser.addOption("Nothing", new ZeroPitch(turret));
   }
-
-    // manipulatorController.whenPressed(XboxController.Trigger.RIGHT_TRIGGER, new GeneralizedReleaseRoutine(indexer, turret));
-    // manipulatorController.whenPressed(XboxController.Trigger.RIGHT_TRIGGER, new GeneralizedReleaseRoutine(indexer, turret));
-    // manipulatorController.whenPressed(XboxController.LEFT_BUMPER, new PassThrough Out);
-    // manipulatorController.whenPressed(XboxController.Button.RIGHT_BUMPER, new RunCommand(
-    // () -> {
-    // indexer.setPower(Constants.Indexer.kIndexerSpeed), indexer;
-    // }
-    // );
-    // manipulatorController.whenPressed(XboxController.Axis.LEFT_Y, new ManualClimber(driverController, climber));
-    // manipulatorController.whenPressed(XboxController.Up, new REzero);
-    // manipulatorController.whenPressed(XboxController.DOWN, new ShootFromLaunchpad);
-    // manipulatorController.whenPressed(XboxController.Button.A, new ClimberDown);
-    // manipulatorController.whenPressed(XboxController.ButtonThinggggg, new Instant)
-    // manipulatorController.whenPressed(XboxController.Button.B, new SequentialCommandGroup(
-    //   new ZeroAndDisable(turret),
-    //   new UpClimber(climber, Constants.Climber.kLowClimb)));
-    // manipulatorController.whenPressed(XboxController.Button.Y, new SequentialCommandGroup(
-    //   new ZeroAndDisable(turret),
-    //   new UpClimber(climber, Constants.Climber.kMidClimb)));
  
-    private void configureButtonBindings(){
+  private void configureButtonBindings(){
     manipulatorController.whenPressed(XboxController.Button.X, new ZeroPitch(turret));
 
     driverController.whileHeld(XboxController.Button.RIGHT_BUMPER, new TurnToAngle(drivetrain, turret, () -> {
-      if(turret.limelight.hasTarget()){
-        return turret.limelight.yawOffset() + drivetrain.getHeading();
-      } else {
-        return drivetrain.getHeading();
-      }
+      return drivetrain.getHeading() + (turret.limelight.hasTarget() ? turret.limelight.yawOffset() : 0);
     }));
 
     manipulatorController.whenPressed(XboxController.Button.B, new InstantCommand(intake::toggleDeploy));
@@ -183,69 +109,8 @@ public class RobotContainer {
   }
 
   public Command getAutonomousCommand() {
-    // The voltage constraint makes sure the robot doesn't exceed a certain voltage during runtime.
-    /*var autoVoltageConstraint = new DifferentialDriveVoltageConstraint(
-        new SimpleMotorFeedforward(
-          Constants.Trajectory.ksVolts, 
-          Constants.Trajectory.ksVoltSecondsPerMeter,
-          Constants.Trajectory.kaVoltSecondsSquaredPerMeter),
-        Constants.Trajectory.kDriveKinematics,10);*/
-
-    // Gives the trajectory the constants determined in characterization.
-    /*TrajectoryConfig config = new TrajectoryConfig(
-      Constants.Trajectory.kMaxSpeedMetersPerSecond,
-      Constants.Trajectory.kMaxAccelerationMetersPerSecondSquared
-    ).setKinematics(Constants.Trajectory.kDriveKinematics).addConstraint(autoVoltageConstraint);    
-
-    // Generates the actual path with given points.
-    /*
-      trajectory = TrajectoryGenerator.generateTrajectory(
-        new Pose2d(0, 0, new Rotation2d(0)),
-          // Pass through these two interior waypoints, making an 's' curve path
-          List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-          // End 3 meters straight ahead of where we started, facing forward
-          new Pose2d(3, 0, new Rotation2d(0)),
-          // Pass config
-          config);
-    */
-            
-    /*try {
-      Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON); 
-      trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
-    } catch (IOException ex) {
-      DriverStation.reportError("Unable to open trajectory: " + trajectoryJSON, ex.getStackTrace());
-    }
-
-    // Ramsete is a trajectory tracker and auto corrector. We feed it parameters into a ramsete command
-    // so that it constantly updates and corrects the trajectory auto.
-   /* RamseteCommand ramseteCommand = new RamseteCommand(
-      trajectory, 
-      drivetrain::getPose, // Gets the translational and rotational position of the robot.
-      new RamseteController(Constants.Trajectory.kRamseteB, Constants.Trajectory.kRamseteZeta),//Uses constants of 2.0 and 0.7
-      new SimpleMotorFeedforward( // Feedforward controller to control the motors before they move
-        Constants.Trajectory.ksVolts,  
-        Constants.Trajectory.ksVoltSecondsPerMeter,
-        Constants.Trajectory.kaVoltSecondsSquaredPerMeter),
-        Constants.Trajectory.kDriveKinematics, 
-        drivetrain::getWheelSpeeds,
-        leftPID,
-        rightPID,
-        drivetrain::tankDriveVolts,
-        drivetrain);
-    drivetrain.resetOdometry(trajectory.getInitialPose());
-    return ramseteCommand.andThen(() -> drivetrain.tankDriveVolts(0,0));
-  */ return autoChooser.getSelected();
-  //return new ShootTwoBalls(drivetrain, turret, indexer, intake); error: "unreachable code"
+    return autoChooser.getSelected();
  }
-
- 
-
- 
-   
-    
-
-
-
 
   public void doSendables(){
     SmartDashboard.putNumber("Gyro Angle", drivetrain.getHeading());
@@ -256,6 +121,5 @@ public class RobotContainer {
     SmartDashboard.putNumber("Climber Encoder2", climber.climbEncoder2.getPosition());
     SmartDashboard.putNumber("Climber Current1", climber.climberMotor1.getOutputCurrent());
     SmartDashboard.putNumber("Climber Current1", climber.climberMotor1.getOutputCurrent());
-
   }
 }
